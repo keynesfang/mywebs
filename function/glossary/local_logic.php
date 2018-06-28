a3e29b22e6e09e0b3c796366d7451665ac0a0beb
zds-fangjun <fangjun@zdsoft.cn>
2018-06-28 14:58:56 +0800

    glossary update

--- function/glossary/local_logic.php
+++ function/glossary/local_logic.php
@@ -3,7 +3,8 @@ require_once("../common_logic.php");
 $type = $_REQUEST['type'];
 
 if ($type == "get_basic_word") {
-    $sql = "SELECT * FROM eng_word order by e_classify";
+    $word_type = $_REQUEST['word_type'];
+    $sql = "SELECT * FROM eng_word where e_classify='$word_type'";
     $result = $dbop->get_all($sql);
     echo json_encode($result);
 }
