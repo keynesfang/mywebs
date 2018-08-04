d0d1a93a544080921d896035ec35d91a7761d0fa
keynes <fangjun@zdsoft.cn>
2018-08-04 09:06:24 +0800

    style

--- function/word_meaning/action.js
+++ function/word_meaning/action.js
@@ -7,7 +7,7 @@ function regist_click(class_name, callback) {
 }
 
 function for_something_show(title) {
-    var html = "<div id='show_meaning' style='position: fixed; left: 1%; width: 98%; border-radius: 5px;' class='bg-success p-2'>";
+    var html = "<div id='show_meaning' style='position: fixed; left: 1%; width: 98%; border-radius: 5px; z-index: 999;' class='bg-info p-2'>";
     html += "<div style='position: relative;'><i class='fa fa-close text-dark' style='position: absolute; right: 10px;' onclick='clear_window();'></i></div>";
     html += "<div id='show_meaning_title' class='text-warning pl-2 pb-1' style='border-bottom: 1px solid #fff;'>" + title + "</div>";
     html += "<div id='show_meaning_content' class='pl-2 mt-2 content'>" + eng_meaning[title].explain + "</div>";
