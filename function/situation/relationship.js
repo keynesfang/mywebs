8872296a9c5f96d7574217a5e2f0053e523c578c
keynes <fangjun@zdsoft.cn>
2018-08-27 22:48:20 +0800

    situation

---
+++ function/situation/relationship.js
@@ -0,0 +1,56 @@
+var situation_relation = {
+    "早上起床": [
+        ["Time to wake up!", "该起床了！"],
+        ["Wake up, you sleepyhead.", "起床了，你这个贪睡鬼。"],
+        ["Rise and shine.", "太阳都晒屁股了。"],
+        ["I'll tickle you if you don't wake up.", "再不起床的话，我要抓痒痒了。"],
+        ["Can I sleep some more?", "我可以多睡一会吗？"],
+        ["I‘m so tired that I can‘t get up.", "我太累了，起不来。"],
+        ["I‘ll count to three. Come on!", "我数到三，快点起床！"],
+        ["You don't have to wake me up. I'm already up.", "你不用叫我也没关系。我已经醒了。"],
+        ["Did you not hear the alarm?", "你没有听到闹钟吗？"],
+        ["Don't doze off. Sit right up!", "不要打瞌睡了。做好！"]
+    ],
+    "机场": [
+        ["I have to catch a connecting flight.", "我必须搭乘转机航班。"],
+        ["Where can I get the boarding pass for MU flight?", "我在哪里可以获得MU航班的登机牌？"],
+        ["Where can I get my baggage?", "在哪里可以领到我的行李？"]
+    ],
+    "出租车": [
+        ["Take me to this address, please.", "请带我去到这个地址。"],
+        ["How much does it cost to take a taxi to this address?", "乘出租车到这个地址需要多少钱？"],
+        ["Anywhere around here is fine.", "到这附近的任何地方都可以。"],
+        ["Keep the change.", "不用找零了。"]
+    ],
+    "酒店": [
+        ["I would like to book a room.", "我想预订一个房间。"],
+        ["I'd like a single room.", "我想要一间单人房。"],
+        ["I'd like to book a double room for 3 nights.", "我想预订一间双人房3晚。"],
+        ["How much is a double room per night?", "双人房每晚多少钱？"],
+        ["I'd like a room with a nice view.", "我想要一个风景优美的房间。"],
+        ["I'm arriving late. Please keep my reservation.", "我晚点了。 请保留我的预订。"],
+        ["When is breakfast served?", "何时提供早餐？"],
+        ["The bed sheets are dirty.", "床单很脏。"],
+        ["I'd like to check out.", "我想退房。"]
+    ],
+    "餐厅": [
+        ["A table for two, please.", "请找张桌子，2个人。"],
+        ["We prefer to sit by the window.", "我们想坐在窗边。"],
+        ["Do you have a menu in Chinese?", "你们有中文菜单吗？"],
+        ["What is the most popular dish here?", "这里最受欢迎的菜是什么？"],
+        ["Could I have the check, please?", "请给我账单好吗?"]
+    ],
+    "购物": [
+        ["May I try this on?", "我可以试一下吗？"],
+        ["Do I need to pay tax?", "我需要纳税吗？"],
+        ["Can I pay by credit card?", "我可以用信用卡付款吗？"],
+        ["It's too expensive.", "这太贵了。"],
+        ["Can you come down a little bit?", "能稍微便宜点吗？"],
+        ["Is this your final price?", "这是你的最终价格吗？"],
+        ["I'd like one size up.", "我想要大一个尺码。"],
+        ["I'll take this one.", "我就要这个了。"],
+        ["Where is the cashier?", "收银员在哪里？"],
+        ["Where can I change money.", "我可以在哪里换钱。"],
+        ["Is there an ATM machine around here?", "这附近有自动取款机吗？"]
+    ]
+};
