d1f8f3ff85d2738b96b96faa10d9c5e032c2e0a5
keynes <fangjun@zdsoft.cn>
2018-09-13 23:07:16 +0800

    up

--- function/study_plan/local_logic.php
+++ function/study_plan/local_logic.php
@@ -8,5 +8,14 @@ if ($type == "get_sound_mark") {
     echo json_encode($result);
 }
 
+if ($type == "study_process_add") {
+    $condition = $_REQUEST['condition']; // a='b' and c='d'
+    $result = $dbop->field_update_itself("eng_user", "study", 1, $condition);
+    if ($result) {
+        echo "success";
+    } else {
+        echo "fail";
+    }
+}
 
 
