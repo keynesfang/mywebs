b8d1596aec28de5bd710d767fdfa2ddedfd8cc85
keynes <fangjun@zdsoft.cn>
2018-11-27 23:02:17 +0800

    up

--- try/index.js
+++ try/index.js
@@ -1,22 +1,42 @@
 var controls = {
     header: undefined,
     menu_package: undefined,
-    tab_package: undefined
+    tab_package: undefined,
+    tabs: undefined
 };
 
 var widgets = {
+    tabs: {
+        id: "tabs",
+        parent_id: "tab_package",
+        tab_list: [
+            {
+                tab_name: "主页"
+            },
+            {
+                tab_name: "子页",
+                close_btn: true
+            }
+        ]
+    },
     menu: {
+        id: "test_menu",
+        parent_id: "menu_package",
         default_func_valid: true,
-        common_func: function () {
+        click_func: function () {
             console.log("menu common function");
         },
         menu_list: [
+            {
+                menu_name: "用户管理",
+                sub_menu_list: ["用户一览", "新增用户", "权限设置"]
+            },
             {
                 menu_name: "菜单一",
                 sub_menu_list: [
                     {
                         sub_menu_name: "子菜单一",
-                        sub_menu_func: "sub_menu_func"
+                        sub_menu_click_func: "sub_menu_click_func"
                     }, "子菜单二", "子菜单三", "子菜单四", "子菜单五"]
             },
             {
@@ -26,42 +46,35 @@ var widgets = {
             {
                 menu_name: "菜单三",
                 sub_menu_list: ["子菜单一", "子菜单二", "子菜单三", "子菜单四", "子菜单五"]
-            },
-            {
-                menu_name: "菜单四",
-                sub_menu_list: ["子菜单一", "子菜单二", "子菜单三", "子菜单四", "子菜单五"]
             }
         ]
     }
 };
 
 function addTab() {
-    var tabs = $("#tabs");
     var tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>";
     var label = "abc",
         id = "tabs-2x",
         li = $(tabTemplate.replace(/#\{href\}/g, "#" + id).replace(/#\{label\}/g, label)),
         tabContentHtml = "lkdjasldkjf";
 
-    tabs.find(".ui-tabs-nav").append(li);
-    tabs.append("<div id='" + id + "'><p>" + tabContentHtml + "</p></div>");
-    tabs.tabs("refresh");
+    controls.tabs.find(".ui-tabs-nav").append(li);
+    controls.tabs.append("<div id='" + id + "'><p>" + tabContentHtml + "</p></div>");
+    controls.tabs.tabs("refresh");
 }
 
-function sub_menu_func() {
+function sub_menu_click_func() {
     console.log("sub menu click!");
     addTab();
 }
 
 $(function () {
     init_widgets();
-    widget_menu.init(widgets.menu, "menu_package", "test_menu");
-    control_assign();
-    control_size_adjust();
-    $("#tabs").tabs();
     $(window).resize(function () {
         control_size_adjust();
     });
+    control_assign();
+    control_size_adjust();
     var tabs = $("#tabs");
     tabs.on("click", "span.ui-icon-close", function () {
         var panelId = $(this).closest("li").remove().attr("aria-controls");
@@ -80,19 +93,20 @@ $(function () {
 
 function init_widgets() {
     $.each(widgets, function (widget) {
-        if(typeof(eval("widget_" + widget)) == "object") {
-            console.log(eval("widget_" + widget));
+        if (typeof(eval("widget_" + widget)) == "object") {
+            eval("widget_" + widget).init(widgets[widget]);
         }
     });
 }
 
 function control_assign() {
-    $.each(controls, function (id, control) {
+    $.each(controls, function (id) {
         controls[id] = $("#" + id);
     });
 }
 
 function control_size_adjust() {
+    var document_width = document.documentElement.clientWidth;
     var document_height = document.documentElement.clientHeight;
     // menu_package
     controls.menu_package.css("height", document_height - controls.header.height() - 10);
@@ -100,5 +114,6 @@ function control_size_adjust() {
     controls.tab_package
         .css("left", controls.menu_package.outerWidth(true))
         .css("top", controls.header.height())
-        .css("height", controls.menu_package.height());
+        .css("height", controls.menu_package.height())
+        .css("width", document_width - controls.tab_package.position().left);
 }
--- try/utility/UIWrapper.js
+++ try/utility/UIWrapper.js
@@ -1,8 +1,45 @@
-var widget_menu = {
-    init: function (menu_struct, parent_id, id) {
-                var parent = $("#" + parent_id);
+var widget_tabs = {
+    tabs_obj: undefined,
+    init: function (obj) {
+        // 根据生成tabs对象，生成HTML
+        widget_tabs.tabs_obj = obj;
+        var id = obj.id;
+        var parent_id = obj.parent_id;
+        var parent = $("#" + parent_id);
         var html = "<div id='" + id + "'>";
-        $.each(menu_struct.menu_list, function (idx, itm) {
+        var tabs_header_html = "<ul>";
+        var tabs_body_html = "";
+        $.each(obj.tab_list, function (idx, itm) {
+            var tab_id = "tab_" + idx;
+            tabs_header_html += "<li><a href='#" + tab_id + "'>" + itm.tab_name + "</a>";
+            var btn_html = "";
+            if (typeof(itm.close_btn) && itm.close_btn) {
+                btn_html = " <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span>";
+            }
+            tabs_header_html += btn_html + "</li>";
+            tabs_body_html += "<div id='" + tab_id + "'>";
+            tabs_body_html += "<p>" + tab_id + "</p>";
+            tabs_body_html += "</div>";
+        });
+        tabs_header_html += "</ul>";
+        html += tabs_header_html + tabs_body_html + "</div>";
+        parent.html(html);
+
+        // 初始化Tabs控件
+        $("#" + id).tabs();
+    }
+};
+
+var widget_menu = {
+    menu_obj: undefined,
+    init: function (obj) {
+        // 根据生成Menu对象，生成HTML
+        widget_menu.menu_obj = obj;
+        var id = obj.id;
+        var parent_id = obj.parent_id;
+        var parent = $("#" + parent_id);
+        var html = "<div id='" + id + "'>";
+        $.each(obj.menu_list, function (idx, itm) {
             html += "<h3>" + itm.menu_name + "</h3>";
             html += "<div>";
             html += "<ul class='sub_menu'>";
@@ -10,33 +47,38 @@ var widget_menu = {
                 if (typeof(sub_itm) == "string") {
                     html += "<li><div><span class='ui-icon ui-icon-battery-3'></span>" + sub_itm + "</div></li>";
                 } else if (typeof(sub_itm) == "object") {
-                    html += "<li func='" + sub_itm.sub_menu_func + "'><div><span class='ui-icon ui-icon-battery-3'></span>" + sub_itm.sub_menu_name + "</div></li>";
+                    html += "<li func='" + sub_itm.sub_menu_click_func + "'><div><span class='ui-icon ui-icon-battery-3'></span>" + sub_itm.sub_menu_name + "</div></li>";
                 }
             });
             html += "</ul></div>";
         });
         html += "</div>";
         parent.html(html);
-
+        // 初始化手风琴控件
         $("#" + id).accordion({
             collapsible: true,
             heightStyle: "content"
         });
-
+        // 初始化菜单控件
         $(".sub_menu").menu({
-            select: function (event, ui) {
-                var selected_menu_item = $(ui.item[0]);
-                if (menu_struct.default_func_valid) {
-                    $(".ui-menu-item").removeClass("menu_selected");
-                    selected_menu_item.addClass("menu_selected");
-                }
-                if (typeof(menu_struct.common_func) == "function") {
-                    menu_struct.common_func();
-                }
-                if (typeof(eval(selected_menu_item.attr("func"))) == "function") {
-                    eval(selected_menu_item.attr("func"))();
-                }
-            }
+            select: widget_menu.menu_click
         });
+    },
+
+    menu_click: function (event, ui) {
+        // 每个菜单的默认执行方法
+        var selected_menu_item = $(ui.item[0]);
+        if (widget_menu.menu_obj.default_func_valid) {
+            $(".ui-menu-item").removeClass("menu_selected");
+            selected_menu_item.addClass("menu_selected");
+        }
+        // Menu对象中定义的菜单共通方法
+        if (typeof(widget_menu.menu_obj.click_func) == "function") {
+            widget_menu.menu_obj.click_func();
+        }
+        // Menu对象中每个子项定义的单击方法
+        if (typeof(eval(selected_menu_item.attr("func"))) == "function") {
+            eval(selected_menu_item.attr("func"))();
+        }
     }
 };
